7f0a6844372aced384c624ae5d8bc1826a2bed20
33123312 <65249051+33123312@users.noreply.github.com>
2022-02-12 12:15:54 -0700

    por favorr

--- systemDwnApp.js
+++ systemDwnApp.js
@@ -5,7 +5,8 @@ let app = express();
 app.listen(3001,()=> console.log("3001"));
 
 app.get('/downloadLauncher', function(req, res){
-    const file = `${__dirname}/sistemaBuilds/SystemLauncher-1.0.jar`;
-    res.download(file);
+    const launcher = `${__dirname}/dwn/SistemaCeb.zip`;
+    res.download(launcher);
+  
   });
 
